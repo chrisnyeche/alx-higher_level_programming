4a2dbe22f5b0291b82acf7ec563922b7d6cb28af
chrisdesignsofficial <chrisdesigns2021@gmail.com>
2022-11-28 23:08:20 +0100

    main

--- 0x00-python-hello_world/10-linked_lists.c
+++ 0x00-python-hello_world/10-linked_lists.c
@@ -6,7 +6,7 @@
  * print_listint - prints all elements of a listint_t list
  * @h: pointer to head of list
  * Return: number of nodes
-*/
+ */
 size_t print_listint(const listint_t *h)
 {
 	const listint_t *current;
@@ -29,7 +29,7 @@ size_t print_listint(const listint_t *h)
  * @head: pointer to a pointer of the start of the list
  * @n: integer to be included in node
  * Return: address of the new element or NULL if it fails
-*/
+ */
 listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
@@ -49,7 +49,7 @@ listint_t *add_nodeint(listint_t **head, const int n)
  * free_listint - frees a listint_t list
  * @head: pointer to list to be freed
  * Return: void
-*/
+ */
 void free_listint(listint_t *head)
 {
 	listint_t *current;
--- 0x00-python-hello_world/lists.h
+++ 0x00-python-hello_world/lists.h
@@ -7,10 +7,10 @@
  * struct listint_s - singly linked list
  * @n: integer
  * @next: points to the next node
- *
+ * 
  * Description: singly linked list node structure
  * for Holberton project
-*/
+ */
 typedef struct listint_s
 {
 	int n;
